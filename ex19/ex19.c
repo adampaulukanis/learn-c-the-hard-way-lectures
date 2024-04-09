17f7dc609f69a993bac475e1a9d66a4e036fe5c5
Adam Paulukanis <adam.paulukanis@gmail.com>
2024-04-10 00:02:10 +0100

    Fix typo s/ex20/ex19/g

--- ex19/ex19.c
+++ ex19/ex19.c
@@ -104,7 +104,7 @@ int main(int argc, char *argv[])
     test_log_warn();
     test_log_info();
 
-    check(test_check("ex20.c") == 0, "failed with ex20.c");
+    check(test_check("ex19.c") == 0, "failed with ex19.c");
     check(test_check(argv[1]) == -1, "failed with argv");
     check(test_sentinel(1) == 0, "test_sentinel failed.");
     check(test_sentinel(100) == -1, "test_sentinel failed.");
